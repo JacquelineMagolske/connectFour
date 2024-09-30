89924aeacd90e05cd5e61554af3ddd0b80fd93fd
jacqueline.magolske <jacqueline.magolske@MU-C1-13.mullen.pvt.k12.co.us>
2024-09-30 13:56:25 -0600

    second commit

--- Connect Four/src/connectFour.java
+++ Connect Four/src/connectFour.java
@@ -2,10 +2,54 @@
 public class connectFour
 	{
 
+		
+		String[][] gameBoard = new String[5][6];
+		
 		public static void main(String[] args)
 			{
+				//greetUser();
+				System.out.println("    1   2   3   4   5   6");
+				System.out.println("  -------------------------");
+				System.out.println("A | " + " " + " | " + " " + " | " + " " + " | " + " " + " | " + " " + " | " + " " + " | ");
+				System.out.println("  -------------------------");
+				System.out.println("B | " + " " + " | " + " " + " | " + " " + " | " + " " + " | " + " " + " | " + " " + " | ");
+				System.out.println("  -------------------------");
+				System.out.println("C | " + " " + " | " + " " + " | " + " " + " | " + " " + " | " + " " + " | " + " " + " | ");
+				System.out.println("  -------------------------");
+				System.out.println("D | " + " " + " | " + " " + " | " + " " + " | " + " " + " | " + " " + " | " + " " + " | ");
+				System.out.println("  -------------------------");
+				System.out.println("E | " + " " + " | " + " " + " | " + " " + " | " + " " + " | " + " " + " | " + " " + " | ");
+				System.out.println("  -------------------------");
 				
 
 			}
-
+		
+		public static void greetUser() 
+			{
+			System.out.println("Hello, welcome to connect four!");
+			}
+		
+		
+		public static void gameboardValues()
+			{
+				
+				
+				System.out.println("    1   2   3   4   5   6");
+				System.out.println("  -------------------------");
+				System.out.println("A | " + " " + " | " + " " + " | " + " " + " | " + " " + " | " + " " + " | " + " " + " | ");
+				System.out.println("  -------------------------");
+				System.out.println("B | " + " " + " | " + " " + " | " + " " + " | " + " " + " | " + " " + " | " + " " + " | ");
+				System.out.println("  -------------------------");
+				System.out.println("C | " + " " + " | " + " " + " | " + " " + " | " + " " + " | " + " " + " | " + " " + " | ");
+				System.out.println("  -------------------------");
+				System.out.println("D | " + " " + " | " + " " + " | " + " " + " | " + " " + " | " + " " + " | " + " " + " | ");
+				System.out.println("  -------------------------");
+				System.out.println("E | " + " " + " | " + " " + " | " + " " + " | " + " " + " | " + " " + " | " + " " + " | ");
+				System.out.println("  -------------------------");
+			}
+		
+		public static void gameBoard()
+			{
+			
+			}
 	}
